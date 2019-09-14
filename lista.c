63f9d0dcda1f21b9503f72dfbf6bda139d81e43f
JerezM <jerez149@gmail.com>
2019-09-14 18:51:49 -0300

    Crear lista & insertar

---
+++ lista.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lista.h"
+
+
+/**
+ Inicializa una lista vacia.
+ Una referencia a la lista creada es referenciada en *L.
+**/
+void crear_lista(tLista * l) {
+    *l= (tLista)malloc(sizeof(struct celda));
+    (*l)->siguiente= NULL;
+    (*l)->elemento= NULL;
+}
+
+/**
+ Inserta el elemento E, en la posicion P, en L.
+ Con L = A,B,C,D y la posicion P direccionando C, luego:
+ L' = A,B,E,C,D
+**/
+void l_insertar(tLista l, tPosicion p, tElemento e) {
+    tPosicion *nuevo;
+    *nuevo=(tPosicion)malloc(sizeof(struct celda));
+
+    (*nuevo)->siguiente= p->siguiente;
+    (*nuevo)->elemento= e;
+    p->siguiente= *nuevo;
+}
+
