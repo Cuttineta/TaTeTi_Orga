dc8f6ac21ba6f30721c97d385eeb4be2f70b2a39
JerezM <jerez149@gmail.com>
2019-09-14 23:14:02 -0300

    Tester v1

--- tester.c
+++ tester.c
@@ -3,16 +3,40 @@
 
 #include "lista.h"
 
-
+#define FIN_TESTER 6
+#define LISTA_CREADA 7
+#define LISTA_NO_CREADA 8
 
 int main()
 {
-    tLista l;
-    crear_lista(&l);
-    int a=3;
-    l_insertar(l,l_primera(l),&a);
-    int *k= l_recuperar(l,l_primera(l));
-    printf("%d",*k);
+    printf("Bienvenido al tester de lista.\n");
+    int decision= 0;//Flag de decisiones.
+    int estadoLista= LISTA_NO_CREADA;//Flag para indicar cuando la lista fue creada.
+    tLista lista= NULL;
+
+    do {
+        printf("Elija una opcion para continuar:\n");
+        printf("1- Crear lista de enteros.\n2- Agregar un entero.\n3- Imprimir lista por pantalla.\n4- Salir.\n");
+        scanf("%d", &decision);
+
+        switch(decision) {
+            case 1:
+            {
+                if(estadoLista==LISTA_NO_CREADA){
+                    crear_lista(&lista);//Se crea la lista
+                    estadoLista= LISTA_CREADA;
+                    printf("Se ha creado la lista satisfactoriamente.\n\n\n\n");
+                }else {
+                    printf("La lista ya se encuentra creada.\n\n\n\n");
+                }
+
+            }
+            break;
+
+
+        }
+
+    } while(decision != FIN_TESTER);
 
     return 0;
 }
