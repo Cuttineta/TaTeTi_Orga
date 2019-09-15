cf6827d4cb68266e2a129f6ee025e660635b6bec
JerezM <jerez149@gmail.com>
2019-09-14 21:01:49 -0300

    modificacion de identacion

--- lista.c
+++ lista.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "lista.h"
 
 
@@ -12,9 +13,9 @@ void crear_lista(tLista * l) {
     if(!(*l)){
         exit(LST_ERROR_MEMORIA);
     }
+
     (*l)->siguiente= NULL;
     (*l)->elemento= NULL;
-    printf("Lista creada padre\n");
 }
 
 /**
@@ -29,12 +30,12 @@ void l_insertar(tLista l, tPosicion p, tElemento e) {
     if(!nuevo){
         exit(LST_ERROR_MEMORIA);
     }
+
     nuevo->siguiente= p->siguiente;
     nuevo->elemento= e;
     p->siguiente= nuevo;
-
-    printf("Insertado \n");
 }
+
 /**
  Elimina el nodo que se encuentra en la posicion P de L.
  El elemento almacenado en la posicion P es eliminado mediante la funcion fEliminar parametrizada.
@@ -46,6 +47,7 @@ void l_insertar(tLista l, tPosicion p, tElemento e) {
  Destruye la lista L, elimininando cada una de sus posiciones. Los elementos almacenados en las posiciones son eliminados mediante la funcion fEliminar parametrizada.
 **/
  void l_destruir(tLista * l, void (*fEliminar)(tElemento));
+
 /**
  Recupera y retorna el elemento en la posicion P.
  Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
@@ -61,26 +63,26 @@ tElemento l_recuperar(tLista l, tPosicion p){
  Recupera y retorna la primera posicion de L.
  Si L es vacia, primera(L) = ultima(L) = fin(L).
 **/
- tPosicion l_primera(tLista l){
-     printf("Primera bro \n");
+tPosicion l_primera(tLista l){
      return l;
- }
+}
 
 /**
  Recupera y retorna la posicion siguiente a P en L.
  Si P es fin(L), finaliza indicando LST_NO_EXISTE_SIGUIENTE.
 **/
- tPosicion l_siguiente(tLista l, tPosicion p){
+tPosicion l_siguiente(tLista l, tPosicion p){
     if((p->siguiente)==NULL){
         exit(LST_NO_EXISTE_SIGUIENTE);
     }
-    return (p->siguiente);}
+    return (p->siguiente);
+}
 
 /**
  Recupera y retorna la posicion anterior a P en L.
  Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
 **/
- tPosicion l_anterior(tLista l, tPosicion p){
+tPosicion l_anterior(tLista l, tPosicion p){
      if(p == l)
         exit(LST_NO_EXISTE_ANTERIOR);
     tPosicion aux=l;
@@ -89,27 +91,28 @@ tElemento l_recuperar(tLista l, tPosicion p){
         aux=aux->siguiente;
 
     return aux;
- }
+}
 
- /**
+/**
  Recupera y retorna la ultima posicion de L.
  Si L es vacia, primera(L) = ultima(L) = fin(L).
 **/
- tPosicion l_ultima(tLista l){
+tPosicion l_ultima(tLista l){
      tPosicion aux=l;
      while(aux->siguiente->siguiente!=NULL)
         aux= aux->siguiente;
 
      return aux;
- }
+}
 
- /**
+/**
  Recupera y retorna la posicion fin de L.
  Si L es vacia, primera(L) = ultima(L) = fin(L).
 **/
- tPosicion l_fin(tLista l){
+tPosicion l_fin(tLista l){
   tPosicion aux=l;
      while(aux->siguiente!=NULL)
         aux= aux->siguiente;
 
-     return aux;}
+     return aux;
+}
